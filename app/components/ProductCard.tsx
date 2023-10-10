04dda2a12f8a23159828bac6341eecf0440fef15
niharika-ctruh <niharika@ctruh.com>
2023-10-11 02:18:25 +0530

    completed

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddToCard from "./AddToCard";
+
+const ProductCard = () => {
+  return (
+    <div className="">
+      <AddToCard />
+    </div>
+  );
+};
+
+export default ProductCard;
